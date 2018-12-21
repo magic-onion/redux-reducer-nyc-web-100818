94ea8c31ccdadab507dbf29154d2189e5a61e6ec
magic-onion <alexander.m.truong@gmail.com>
2018-12-20 19:12:29 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,21 @@
 export function manageFriends(state, action){
+
+  switch (action.type) {
+
+    case 'ADD_FRIEND':
+      return {friends: state.friends.concat(action.friend)}
+
+    case 'REMOVE_FRIEND':
+
+    const newFriends = state => {
+      let index = state.friends.indexOf(state.friends.find(friend=> friend.id === action.id))
+      const newFriends = [...state.friends]; newFriends.splice(index, 1);
+      return newFriends
+    }
+
+    return {friends: newFriends(state)}
+
+    default:
+      return state;
+  }
 }
